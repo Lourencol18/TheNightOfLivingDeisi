34d9df057a0ce54681ee9830b1e011540dd20831
Lourenço <a22406954@alunos.ulht.pt>
2024-10-24 17:30:07 +0100

    Adicionei classes novas

--- src/pt/ulusofona/lp2/thenightofthelivingdeisi/Zombie.java
+++ src/pt/ulusofona/lp2/thenightofthelivingdeisi/Zombie.java
@@ -2,22 +2,21 @@ package pt.ulusofona.lp2.thenightofthelivingdeisi;
 
 import java.util.ArrayList;
 
-public class Humano {
+public class Zombie {
     int id;
     String TipoCriatura;
     String nome;
     ArrayList<Integer> coordenadas;
     Equipamento equipamento;
 
-    public Humano(int id, String tipoCriatura, ArrayList<Integer> coordenadas, String nome, Equipamento equipamento) {
+    public Zombie(int id, String tipoCriatura, String nome, ArrayList<Integer> coordenadas, Equipamento equipamento) {
         this.id = id;
         TipoCriatura = tipoCriatura;
-        this.coordenadas = coordenadas;
         this.nome = nome;
+        this.coordenadas = coordenadas;
         this.equipamento = equipamento;
     }
-
-    public void apanharEquipamento() {
+    public void destruirEquipamento(){
 
     }
 }
