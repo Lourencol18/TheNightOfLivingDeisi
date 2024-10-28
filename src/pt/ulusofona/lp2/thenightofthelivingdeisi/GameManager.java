ad1da782a9d39858028fa472b08e3ec5fe7e2aa2
Ruben Graça <a22307816@alunos.ulht.pt>
2024-10-28 16:23:57 +0000

    equipementinfoasstring

--- src/pt/ulusofona/lp2/thenightofthelivingdeisi/GameManager.java
+++ src/pt/ulusofona/lp2/thenightofthelivingdeisi/GameManager.java
@@ -210,7 +210,16 @@ public class GameManager {
     }
 
     public String getEquipmentInfoAsString(int id){
-        return "ola";
+        public String getEquipmentInfoAsString(int id) {
+            for (Equipamento equipment : equipamentos) {
+                if (equipment.getId() == id) {
+                    String tipoEquipamento = (equipment.tipo == 0) ?
+                            "Escudo de madeira" : "Espada samurai";
+                    return id + " | " + tipoEquipamento + " @ (" + equipment.getX() + "," + equipment.getY() + ")";
+                }
+            }
+            return null; // Retorna null se o equipamento não for encontrado
+        }
     }
 
     public boolean hasEquipment(int creatureId, int equipmentTypeId){
