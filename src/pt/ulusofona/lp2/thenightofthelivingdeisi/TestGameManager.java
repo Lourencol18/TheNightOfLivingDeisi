54232e6831f4fed01689c49fc1a9604b9baf0a49
Lourenço <a22406954@alunos.ulht.pt>
2024-12-11 15:29:43 +0000

    testes

--- src/pt/ulusofona/lp2/thenightofthelivingdeisi/TestGameManager.java
+++ src/pt/ulusofona/lp2/thenightofthelivingdeisi/TestGameManager.java
@@ -1,13 +1,4 @@
 package pt.ulusofona.lp2.thenightofthelivingdeisi;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.util.List;
-
-import static org.junit.Assert.*;
-
 public class TestGameManager {
-
 }
