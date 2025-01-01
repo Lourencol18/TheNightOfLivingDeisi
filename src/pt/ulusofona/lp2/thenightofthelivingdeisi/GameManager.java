a920f3f8f6d1b2babb8ab887e9a796e77f508709
Lourenço <a22406954@alunos.ulht.pt>
2025-01-01 18:13:11 +0000

    correçao 9.0

--- src/pt/ulusofona/lp2/thenightofthelivingdeisi/GameManager.java
+++ src/pt/ulusofona/lp2/thenightofthelivingdeisi/GameManager.java
@@ -965,7 +965,7 @@ public class GameManager {
     public boolean gameIsOver() {
         if (turnosSemEventos >= 8) {
             return true;
-        } else if (invalidMovesHumanos >= 10 || invalidMovesZombies >= 10) {
+        } else if (invalidMovesHumanos >=5 || invalidMovesZombies >= 5) {
             return true;
         }
 
